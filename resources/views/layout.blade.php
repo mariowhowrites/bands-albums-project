b5e860ac719b169c503059ca847bdd94f255bfa5
Mario Vega <mario@ark.io>
2019-04-28 18:27:52 -0700

    change website title

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,11 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Bands and Albums Project</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
         <!-- Styles -->
         <link href="/css/app.css" rel="stylesheet">
     </head>
